4fa8000dd11813ed913fc0c46f18389ee40b7e95
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-06-24 04:49:09 +0530

    has set up postgres , express ,nodejs

---
+++ server.js
@@ -0,0 +1,14 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+require("./config/db");
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
