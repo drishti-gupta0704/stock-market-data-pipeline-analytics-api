d260b13c2153a63b4ae5abbc1cd3ef6b09221912
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-06-25 04:15:25 +0530

    transformed and validated raw API data and passed to controller

--- services/stockService.js
+++ services/stockService.js
@@ -1,5 +1,6 @@
 
 const axios = require("axios");
+const { transformStockData } = require("../utils/stockTransformer");
 
 const API_KEY = process.env.ALPHA_VANTAGE_API_KEY;
 
@@ -9,19 +10,16 @@ const getStockData = async (symbol) => {
 
     const response = await axios.get(url);
 
-    const data = response.data["Global Quote"];
+    const rawData = response.data["Global Quote"];
 
-    if (!data) {
+    if (!rawData) {
       throw new Error("Invalid API response");
     }
 
-    return {
-      symbol: data["01. symbol"],
-      price: parseFloat(data["05. price"]),
-      change: parseFloat(data["09. change"]),
-      percentChange: data["10. change percent"],
-      timestamp: new Date()
-    };
+    const transformedData =  transformStockData(rawData);
+    
+    return transformedData;
+  
 
   } catch (error) {
     console.error("Stock API Error:", error.message);
---
+++ utils/stockTransformer.js
@@ -0,0 +1,25 @@
+
+const transformStockData = (data) => {
+
+  if (!data["01. symbol"]) {
+    throw new Error("Symbol missing");
+  }
+
+  if (!data["05. price"]) {
+    throw new Error("Price missing");
+  }
+
+  return {
+    symbol: data["01. symbol"],
+
+    price: parseFloat(data["05. price"]),
+
+    priceChange: parseFloat(data["09. change"]),
+
+    percentChange: data["10. change percent"],
+
+    timestamp: new Date()
+  };
+};
+
+module.exports = { transformStockData };
