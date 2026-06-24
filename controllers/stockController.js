445e22b960bed1b7ad7b5624fc37130d45ae8df5
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-06-24 20:30:13 +0530

    extracting symbol from requestobject and passing it to services folder

---
+++ controllers/stockController.js
@@ -0,0 +1,27 @@
+
+const { getStockData } = require("../services/stockService");
+
+const fetchStock = async (req, res) => {
+  try {
+    const symbol = req.params.symbol;
+
+    if (!symbol) {
+      return res.status(400).json({ error: "Symbol is required" });
+    }
+
+    const data = await getStockData(symbol);
+
+    res.json({
+      success: true,
+      data
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch stock data"
+    });
+  }
+};
+
+module.exports = { fetchStock };
