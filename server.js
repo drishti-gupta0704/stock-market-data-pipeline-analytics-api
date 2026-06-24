7d7ad68f04f06ce8cb62cc8951150f43eac19bdc
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-06-24 20:32:19 +0530

    adding routes to main server file

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require("./config/db");
 
 app.use(express.json());
 
+const stockRoutes = require("./routes/stockRoutes");
+app.use("/stocks", stockRoutes);
+
 app.get("/", (req, res) => {
   res.send("Stock API Running");
 });
