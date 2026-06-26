b82b234df30ce3666a5b5b1f1f18216dc21f98eb
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-06-26 18:50:18 +0530

    implementing cron schedule for automatic data extraction and looping over all symbols of stock

---
+++ cron/stockCron.js
@@ -0,0 +1,38 @@
+
+const cron = require("node-cron");
+const { getStockData } = require("../services/stockService");
+
+const STOCK_SYMBOLS = [
+  "AAPL",
+  "MSFT",
+  "GOOGL",
+  "AMZN",
+  "TSLA"
+];
+
+const startStockCron = () => {
+
+  cron.schedule("*/5 * * * *", async () => {
+
+    console.log("Running Stock Cron Job...");
+
+    for (const symbol of STOCK_SYMBOLS) {
+
+      try {
+        const stock = await getStockData(symbol);
+        console.log(`${symbol} stored successfully`);
+
+      } catch (err) {
+        console.error(`${symbol}: ${err.message}`);
+
+      }
+
+    }
+
+  });
+
+};
+
+module.exports = {
+  startStockCron
+};
